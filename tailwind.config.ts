6fb1d85fc1b6f8158811bf0752169ca62387c080
HD1511 <harshdob937@gmail.com>
2024-01-16 00:37:53 +0530

    Initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,6 @@ import type { Config } from 'tailwindcss';
 const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
-  corePlugins: {
-    preflight: false,
-  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
